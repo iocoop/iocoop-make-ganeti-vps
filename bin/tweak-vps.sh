da3957b5d21f314944caf019c6ddeed1b19a118d
Ben Kochie <superq@gmail.com>
2017-11-13 20:38:34 +0100

    Add sleep after kpartx.

--- bin/tweak-vps.sh
+++ bin/tweak-vps.sh
@@ -86,6 +86,7 @@ if [ ! -h "${instance_disk}" ] ; then
 fi
 
 target_dev="/dev/mapper/$(kpartx -av "${instance_disk}" | awk '{print $3}')"
+sleep 2
 echo "INFO: Attempting to mount '${target_dev}'"
 mount "${target_dev}" "${target}"
 
